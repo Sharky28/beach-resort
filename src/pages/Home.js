232399077e6af6fe5592e49a4334b220aa26137d
SharkyUgas <sharmarke.ugas@outlook.com>
2019-07-14 14:52:47 +0100

    added banners

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import {Link} from 'react-router-dom'
+import Banner from '../components/Banner';
 
 export default function Home() {
-    return <Hero></Hero>
+    return <Hero>
+        <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+            <Link to ="/rooms" className="btn-primary">
+                our rooms
+            </Link>
+        </Banner>
+    </Hero>
 }
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import Hero from '../components/Hero';
+import {Link} from 'react-router-dom'
+import Banner from '../components/Banner';
 
 export default function Rooms() {
     return (
-      <Hero hero="roomsHero"></Hero>
+      <Hero hero="roomsHero">
+          <Banner title ="our rooms">
+            <Link to="/" className="btn-primary">
+                return home
+            </Link>
+          </Banner>
+      </Hero>
     )
 }
