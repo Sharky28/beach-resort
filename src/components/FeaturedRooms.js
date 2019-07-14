162edc7c32909b34f4927cc4b9f75b20fd05ce9c
SharkyUgas <sharmarke.ugas@outlook.com>
2019-07-14 18:06:48 +0100

    started feaured rooms and room componenents

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react'
-import {RoomContext} from '../context';
+import { RoomContext } from '../context';
+import Title from './Title';
+import Loading from './Loading'
+import Room from './Room'
 
 export default class FeaturedRooms extends Component {
     static contextType = RoomContext;
 
     render() {
-        const {name,greeting} = this.context;
-     
-        
+        let { loading, featuredRooms: rooms } = this.context;
+        rooms = rooms.map(room => {
+            return <Room key={room.id} room={room}></Room>
+        })
+
         return (
-            <div>
-                {greeting} {name} hello from featured rooms
-            </div>
+            <>
+                <section className="featured-rooms">
+                    <Title title="featured rooms"></Title>
+                    <div className="featured-rooms-center">
+                        {loading?<Loading></Loading>:rooms}
+                    </div>
+                </section>
+            </>
         )
     }
 }
