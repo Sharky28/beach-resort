685cef24b89a85a14542b0c21931918b8abba3d5
SharkyUgas <sharmarke.ugas@outlook.com>
2019-07-14 15:15:00 +0100

    added services section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import Hero from '../components/Hero'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Banner from '../components/Banner';
+import Services from '../components/Services';
 
 export default function Home() {
-    return <Hero>
-        <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
-            <Link to ="/rooms" className="btn-primary">
-                our rooms
+    return (
+        <>
+            <Hero>
+                <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+                    <Link to="/rooms" className="btn-primary">
+                        our rooms
             </Link>
-        </Banner>
-    </Hero>
+                </Banner>
+            </Hero>
+            <Services></Services>
+        </>
+    );
 }
