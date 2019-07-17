d3e8a74e49d803b27bf04363543ba983bb22140a
SharkyUgas <sharmarke.ugas@outlook.com>
2019-07-17 16:01:34 +0100

    completed roomLIst component

---
+++ src/components/RoomsContainer.js
@@ -0,0 +1,60 @@
+
+import React from 'react'
+import RoomsFilter from './RoomsFilter'
+import RoomsList from './RoomsList'
+import { withRoomConsumer } from '../context'
+import Loading from './Loading'
+
+
+
+function RoomContainer({ context }) {
+
+    const { loading, sortedRooms, rooms } = context;
+
+    if (loading) {
+        return <Loading></Loading>
+    }
+
+    return (
+        <>       
+            <RoomsFilter rooms={rooms}></RoomsFilter>
+            <RoomsList rooms={sortedRooms}></RoomsList>
+        </>
+    )
+}
+
+export default withRoomConsumer(RoomContainer)
+
+
+
+
+
+// import React from 'react'
+// import RoomsFilter from './RoomsFilter'
+// import RoomsList from './RoomsList'
+// import { RoomConsumer } from '../context'
+// import Loading from './Loading'
+// import { log } from 'util';
+// export default function RoomsContainer() {
+//     return (
+//         <RoomConsumer>
+//             {value => {
+
+//                 const {loading,sortedRooms,rooms} = value;
+
+//                 if(loading){
+//                     return <Loading></Loading>
+//                 }
+
+//                 return (
+//                     <div>
+//                         hello from rooms container
+//                        <RoomsFilter rooms ={rooms}></RoomsFilter>
+//                        <RoomsList rooms={sortedRooms}></RoomsList>
+//                     </div>
+//                 )
+//             }}
+//         </RoomConsumer>
+
+//     )
+// }
